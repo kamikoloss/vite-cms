57125edfa92bd1db576b462fde6282640ec3bfba
Kamiko Loss <info@gloxi.net>
2024-04-03 23:51:05 +0900

    メソッドを POST 限定にしてルートで処理を判定するようにした

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -16,6 +16,11 @@ export default {
 		const { url, method } = request;
 		const reqBody: ReqBody = await request.json();
 
+		// POST 限定
+		if (method !== 'POST') {
+			return Response.json({ error: 'invalid method.' })
+		}
+
 		// ルートから KV (pages or blog) を判定する
 		let kv: KVNamespace = env.pages;
 		if (url.includes('/pages')) {
@@ -26,22 +31,31 @@ export default {
 			return Response.json({ error: 'invalid route.' })
 		}
 
-		// メソッドに紐づく処理を行う
-		if (method === 'GET') {
+		// ルートから処理を判定する
+		if (url.includes('/list')) {
+			// キーの一覧を取得する
+			const data = await kv.list();
+			return Response.json({ data });
+		} else if (url.includes('/get')) {
+			// 取得する
 			if (!reqBody.id) return Response.json({ error: 'id is required.' });
-			const data = kv.get(reqBody.id);
-			return Response.json({ data, reqBody });
-		} else if (method === 'POST') {
+			const data = await kv.get(reqBody.id);
+			if (!data) return Response.json({ error: 'not found.' });
+			return Response.json({ data: JSON.parse(data) });
+		} else if (url.includes('/put')) {
+			// 作成する
 			const id = crypto.randomUUID();
-			kv.put(id, JSON.stringify({ ...reqBody }));
-		} else if (method === 'PUT') {
+			await kv.put(id, JSON.stringify({ ...reqBody }));
+		} else if (url.includes('/update')) {
+			// 更新する
 			if (!reqBody.id) return Response.json({ error: 'id is required.' });
-			kv.put(reqBody.id, JSON.stringify({ ...reqBody }));
-		} else if (method === 'DELETE') {
+			await kv.put(reqBody.id, JSON.stringify({ ...reqBody }));
+		} else if (url.includes('/delete')) {
+			// 削除する
 			if (!reqBody.id) return Response.json({ error: 'id is required.' });
-			kv.delete(reqBody.id);
+			await kv.delete(reqBody.id);
 		} else {
-			return Response.json({ error: 'invalid method.' })
+			return Response.json({ error: 'invalid route.' })
 		}
 
 		return Response.json({ message: 'Success!'});

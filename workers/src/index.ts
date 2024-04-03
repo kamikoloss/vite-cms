90093e2147b895cfa78665f71f7daf4ad9aab747
Kamiko Loss <info@gloxi.net>
2024-04-04 02:56:16 +0900

    バリデーションを実装した

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -1,43 +1,50 @@
-export interface Env {
-	pages: KVNamespace;
-}
+export type Env = { pages: KVNamespace; }
+export type ReqType = 'list' | 'get' | 'put' | 'update' | 'delete';
+export type ErrorResBody = { error: string[]; }
 
 // (key) と (value) 以外はすべて metadata
-export interface ReqBody {
-	id?: string, // <PREFIX>-<UUID?> (key)
+export type ReqBody = {
+	id?: string, // (key) <NAME> or <NAME>-<UUID>
 	title: string,
 	type: number,
 	draft: number,
 	created: number, // Unixtime
 	updated: number, // Unixtime
-	body: string, // 本文 (value)
-}
+	body: string, // (value) 本文
+};
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { url, method } = request;
+		const reqType = await getReqType(url);
 		const reqBody: ReqBody = await request.json();
 		const { id, title, type, draft, created, updated, body } = reqBody;
-		const kv: KVNamespace = env.pages;
 
-		// POST 以外の場合: エラー
+		// リクエストのメソッドが POST 以外の場合: エラー
 		if (method !== 'POST') {
-			return Response.json({ error: 'invalid method.' })
+			return Response.json({ error: ['invalid method.'] });
 		}
+		// リクエストのルートが不正な場合: エラー
+		if (!reqType) {
+			return Response.json({ error: ['invalid route.'] });
+		}
+		// リクエストの内容がバリデーションに失敗した場合: エラー
+		const errorResBody = await validateRequest(reqType, reqBody);
+		if (errorResBody) {
+			return Response.json(errorResBody);
+		}
+
+		const kv = env.pages;
 
 		// ルートから処理を判定する
 		if (url.includes('/list')) {
 			// アイテム一覧を取得する
 			const data = await kv.list();
-			return Response.json({ data });
+			return Response.json({ ...data });
 		} else if (url.includes('/get')) {
 			// アイテムを取得する
-			if (!id) return Response.json({ error: 'id is required.' });
-			const data = await kv.getWithMetadata(id);
-			return Response.json({
-				body: JSON.parse(data.value ?? ''),
-				metadata: data.metadata,
-			});
+			const data = await kv.getWithMetadata(id!);
+			return Response.json({ body: data.value, metadata: data.metadata });
 		} else if (url.includes('/put')) {
 			// アイテムを作成する
 			const key = crypto.randomUUID();
@@ -45,18 +52,49 @@ export default {
 			await kv.put(key, JSON.stringify(body), { metadata });
 		} else if (url.includes('/update')) {
 			// アイテムを更新する
-			if (!id) return Response.json({ error: 'id is required.' });
 			const metadata = { title, type, draft, created, updated };
-			await kv.put(id, JSON.stringify({ ...reqBody }), { metadata });
+			await kv.put(id!, JSON.stringify({ ...reqBody }), { metadata });
 		} else if (url.includes('/delete')) {
 			// アイテムを削除する
-			if (!id) return Response.json({ error: 'id is required.' });
-			await kv.delete(id);
-		} else {
-			// それ以外のルート: エラー
-			return Response.json({ error: 'invalid route.' });
+			await kv.delete(id!);
 		}
 
 		return Response.json({ message: 'success!'});
+
+		// リクエストの種別を取得する
+		async function getReqType(url: string): Promise<ReqType | null> {
+			return url.includes('/list') ? 'list'
+				: url.includes('/get') ? 'get'
+				: url.includes('/put') ? 'put'
+				: url.includes('/update') ? 'update'
+				: url.includes('/delete') ? 'delete'
+				: null;
+		}
+
+		// リクエストをバリデーションする
+		async function validateRequest(reqType: ReqType, reqBody: ReqBody): Promise<ErrorResBody | null> {
+			const { id, title, type, draft, created, updated, body } = reqBody;
+			const messages: string[] = [];
+
+			// (key) id
+			if (!id && ['get', 'update', 'delete'].includes(reqType)) {
+				messages.push('id is required.');
+			}
+			// (metadata) title, type, draft, created, updated
+			const hasNoMetadata = (!title || !type || !draft || !created || !updated);
+			if (hasNoMetadata && ['put', 'update'].includes(reqType)) {
+				messages.push('metadata is required.');
+			}
+			// (value) body
+			if (!body && ['put', 'update'].includes(reqType)) {
+				messages.push('body is required.');
+			}
+
+			if (messages.length > 0) {
+				return { error: messages };
+			} else {
+				return null;
+			}
+		}
 	},
 };

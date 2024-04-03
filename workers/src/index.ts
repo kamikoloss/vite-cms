fcc4869cf530882a57d9c2555dee4302f74b8439
Kamiko Loss <info@gloxi.net>
2024-04-03 23:39:18 +0900

    workers に REST API をざっくり実装した

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -1,32 +1,49 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	pages: KVNamespace;
+	blog: KVNamespace;
 }
 
+export interface ReqBody {
+	id?: string,
+	title: string,
+	body: string,
+}
+
+// GET|POST|PUT|DELETE /pages
+// GET|POST|PUT|DELETE /blog
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		const { url, method } = request;
+		const reqBody: ReqBody = await request.json();
+
+		// ルートから KV (pages or blog) を判定する
+		let kv: KVNamespace = env.pages;
+		if (url.includes('/pages')) {
+			kv = env.pages;
+		} else if (url.includes('/blog')) {
+			kv = env.blog;
+		} else {
+			return Response.json({ error: 'invalid route.' })
+		}
+
+		// メソッドに紐づく処理を行う
+		if (method === 'GET') {
+			if (!reqBody.id) return Response.json({ error: 'id is required.' });
+			const data = kv.get(reqBody.id);
+			return Response.json({ data, reqBody });
+		} else if (method === 'POST') {
+			const id = crypto.randomUUID();
+			kv.put(id, JSON.stringify({ ...reqBody }));
+		} else if (method === 'PUT') {
+			if (!reqBody.id) return Response.json({ error: 'id is required.' });
+			kv.put(reqBody.id, JSON.stringify({ ...reqBody }));
+		} else if (method === 'DELETE') {
+			if (!reqBody.id) return Response.json({ error: 'id is required.' });
+			kv.delete(reqBody.id);
+		} else {
+			return Response.json({ error: 'invalid method.' })
+		}
+
+		return Response.json({ message: 'Success!'});
 	},
 };
